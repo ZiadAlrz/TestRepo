b545db9e32df01b66d549b14c531cb013685766a
Ziad Alrez <ziad.alrez@hotmail.com>
2025-02-22 10:19:57 +0100

    first initialization

---
+++ main.c
@@ -0,0 +1,24 @@
+#include "string.h"
+#include "stdio.h"
+#include "main.h"
+// int main(void);
+int multip(int a, int p);
+
+int call_main()
+{
+    main();
+}
+
+void main(void)
+{
+    int x,y;
+    x = test_const;
+    y = multip(3,4);
+    printf("%d 3time4 is equal to %f",test_const,y);
+    return;
+}
+
+int multip(int a, int p)
+{
+    return a*p;
+}
